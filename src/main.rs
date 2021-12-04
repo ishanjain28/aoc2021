e79094fc003e43510fb34606d610cbb1b2694078
Ishan Jain <contact@ishanjain.me>
2021-12-04 11:11:16 +0530

    Added Day 4 Part 1

--- src/main.rs
+++ src/main.rs
@@ -2,94 +2,128 @@
 extern crate test;
 
 const INPUTS: [&'static str; 2] = [
-    include_str!("../inputs/day3.sample.txt"),
-    include_str!("../inputs/day3.txt"),
+    include_str!("../inputs/day4.sample.txt"),
+    include_str!("../inputs/day4.txt"),
 ];
 
-fn cmds(input: &'static str) -> Vec<u64> {
-    input
-        .lines()
-        .filter(|&x| !x.is_empty())
-        .map(|x| u64::from_str_radix(x, 2).unwrap())
-        .collect()
+#[derive(Copy, Clone, Debug)]
+enum BoardEntry {
+    Entry(u64),
+    CalledEntry(u64),
+    Null,
 }
 
-fn solution<const MAX_LENGTH: usize>(levels: Vec<u64>) -> u64 {
-    let oxygen_gen_rating = filter_candidates::<MAX_LENGTH>(levels.clone(), true);
-    let co2_scrub_rating = filter_candidates::<MAX_LENGTH>(levels, false);
-
-    oxygen_gen_rating * co2_scrub_rating
+#[derive(Copy, Clone, Debug)]
+struct Board {
+    board: [[BoardEntry; 5]; 5],
+    sum: u64,
 }
 
-fn filter_candidates<const MAX_LENGTH: usize>(mut levels: Vec<u64>, dominant: bool) -> u64 {
-    let mut i = 0;
-    while levels.len() > 1 {
-        let count = count_bits::<MAX_LENGTH>(&levels, dominant);
-        let leading = count & (1 << (MAX_LENGTH - i - 1));
+fn cmds(input: &'static str) -> (Vec<u64>, Vec<Board>) {
+    let mut lines = input.split("\n\n").filter(|x| !x.is_empty());
 
-        levels = levels
-            .into_iter()
-            .filter(|&x| {
-                let pos = x & (1 << (MAX_LENGTH - i - 1));
+    let draws: Vec<u64> = lines
+        .next()
+        .unwrap()
+        .split(',')
+        .map(|x| x.parse::<u64>().unwrap())
+        .collect();
 
-                return pos == leading;
-            })
-            .collect();
+    let mut boards = vec![];
 
-        i += 1;
+    for block in lines {
+        let mut bblock = Board {
+            board: [[BoardEntry::Null; 5]; 5],
+            sum: 0,
+        };
+
+        for (i, line) in block.split('\n').enumerate() {
+            for (j, c) in line.split(' ').filter(|x| !x.is_empty()).enumerate() {
+                let num = c.parse::<u64>().unwrap();
+                bblock.board[i][j] = BoardEntry::Entry(num);
+                bblock.sum += num;
+            }
+        }
+
+        boards.push(bblock)
     }
 
-    levels[0]
+    (draws, boards)
 }
 
-fn count_bits<const MAX_LENGTH: usize>(levels: &[u64], dominant: bool) -> u64 {
-    let n = levels.len();
-    let mut count = [0; MAX_LENGTH];
-    let mut mask = 0;
+fn solution((draws, mut boards): (Vec<u64>, Vec<Board>)) -> u64 {
+    for draw in draws {
+        // Mark boards
 
-    for &level in levels.iter() {
-        for i in 0..MAX_LENGTH {
-            let c = level & (1 << (MAX_LENGTH - i - 1));
+        mark_entry(&mut boards, draw);
 
-            if c != 0 {
-                count[i as usize] += 1;
+        if let Some(board) = check_winner(&boards) {
+            return board.sum * draw;
+        }
+    }
+
+    0
+}
+
+fn check_winner(boards: &[Board]) -> Option<&Board> {
+    for board in boards {
+        let grid = board.board;
+
+        for i in 0..5 {
+            if grid[i]
+                .iter()
+                .all(|x| matches!(x, BoardEntry::CalledEntry(_)))
+            {
+                return Some(board);
+            }
+        }
+
+        for j in 0..5 {
+            let mut all_called = true;
+            for i in 0..5 {
+                if !matches!(grid[i][j], BoardEntry::CalledEntry(_)) {
+                    all_called = false;
+                    break;
+                }
+            }
+
+            if all_called {
+                return Some(board);
             }
         }
     }
 
-    for (i, c) in count.into_iter().enumerate() {
-        if c >= n - c {
-            mask |= 1 << (MAX_LENGTH - i - 1);
-        }
-    }
+    None
+}
 
-    if dominant {
-        mask
-    } else {
-        !mask
+fn mark_entry(boards: &mut [Board], called: u64) {
+    for board in boards {
+        for row in board.board.iter_mut() {
+            for col in row.iter_mut() {
+                if let BoardEntry::Entry(val) = col {
+                    if *val == called {
+                        board.sum -= *val;
+                        *col = BoardEntry::CalledEntry(*val);
+                    }
+                }
+            }
+        }
     }
 }
 
 fn main() {
-    let moves = cmds(INPUTS[0]);
-    let count = solution::<5>(moves);
-    println!("Result {}", count);
-    let moves = cmds(INPUTS[1]);
-    let count = solution::<12>(moves);
-    println!("Result {}", count);
+    for input in INPUTS {
+        let input = cmds(input);
+        let result = solution(input);
+        println!("Result {}", result);
+    }
 }
 
 #[bench]
 fn solution_bench(b: &mut test::Bencher) {
-    let moves = cmds(INPUTS[1]);
     b.iter(|| {
-        let v = solution::<12>(moves.clone());
-        test::black_box(v);
+        let input = cmds(INPUTS[1]);
+        let result = solution(input);
+        test::black_box(result);
     })
-}
-
-#[test]
-fn solution_test() {
-    assert_eq!(solution::<5>(cmds(INPUTS[0])), 230);
-    assert_eq!(solution::<12>(cmds(INPUTS[1])), 4406844);
 }
