79e5832e94b5ef6c40aa704b05fd3c1efb6574a2
Ishan Jain <contact@ishanjain.me>
2021-12-07 14:23:21 +0530

    Added Day 7 Part 1

--- src/main.rs
+++ src/main.rs
@@ -6,45 +6,61 @@ const INPUTS: [&'static str; 2] = [
     include_str!("../inputs/input.txt"),
 ];
 
-fn parse_input(input: &'static str) -> Vec<u8> {
+fn parse_input(input: &'static str) -> Vec<i64> {
     input
         .split(',')
         .filter(|&x| !x.is_empty())
         .map(|x| x.trim())
-        .map(|x| x.parse::<u8>().unwrap())
+        .map(|x| i64::from_str_radix(x, 10).unwrap())
         .collect()
 }
 
-fn solution<const N: usize>(fishes: Vec<u8>) -> u64 {
-    let mut fish_stat = [0u64; 9];
-    for fish in fishes {
-        fish_stat[fish as usize] += 1;
+#[inline]
+fn compute_cost(crabs: &[i64], mid: i64) -> i64 {
+    let mut cost = 0;
+
+    for &crab in crabs {
+        let distance = (crab - mid).abs();
+
+        cost += distance;
+    }
+    cost
+}
+
+fn solution(crab_pos: Vec<i64>) -> i64 {
+    // Ternary search on crab_pos
+    // Since the solution will always be somewhere in the middle of
+    // the lowest and highest values present in crab_pos
+    let mut low = std::i64::MAX;
+    let mut high = std::i64::MIN;
+
+    for &crab in crab_pos.iter() {
+        low = std::cmp::min(low, crab);
+        high = std::cmp::max(high, crab);
     }
 
-    // Unrolled loop to improve performance
-    let blocks = N / 9;
-    for _ in 0..blocks {
-        fish_stat[7] += fish_stat[0];
-        fish_stat[8] += fish_stat[1];
-        fish_stat[0] += fish_stat[2];
-        fish_stat[1] += fish_stat[3];
-        fish_stat[2] += fish_stat[4];
-        fish_stat[3] += fish_stat[5];
-        fish_stat[4] += fish_stat[6];
-        fish_stat[5] += fish_stat[7];
-        fish_stat[6] += fish_stat[8];
-    }
-    for day in blocks * 9..N {
-        fish_stat[(day + 7) % 9] += fish_stat[day % 9];
+    // Break out when the difference is less than 2 because it'll get stuck in a infinite loop
+    while high - low > 2 {
+        let mid1 = low + (high - low) / 3;
+        let mid2 = high - (high - low) / 3;
+
+        let cost1 = compute_cost(&crab_pos, mid1);
+        let cost2 = compute_cost(&crab_pos, mid2);
+
+        if cost1 < cost2 {
+            high = mid2;
+        } else {
+            low = mid1;
+        }
     }
 
-    fish_stat.into_iter().sum::<u64>()
+    compute_cost(&crab_pos, (low + high) / 2)
 }
 
 fn main() {
     for input in INPUTS {
         let input = parse_input(input);
-        let result = solution::<256>(input);
+        let result = solution(input);
         println!("Result {}", result);
     }
 }
@@ -53,7 +69,7 @@ fn main() {
 fn solution_bench(b: &mut test::Bencher) {
     let input = parse_input(INPUTS[1]);
     b.iter(|| {
-        let result = solution::<256>(input.clone());
+        let result = solution(input.clone());
         test::black_box(result);
     })
 }
