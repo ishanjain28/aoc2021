ea098fbff3f153d56d8dac7ba427ed138690db5c
Ishan Jain <contact@ishanjain.me>
2021-12-08 18:51:58 +0530

    Added Day 8 Part 1

--- src/main.rs
+++ src/main.rs
@@ -6,55 +6,82 @@ const INPUTS: [&'static str; 2] = [
     include_str!("../inputs/input.txt"),
 ];
 
-fn parse_input(input: &'static str) -> Vec<i64> {
-    input
-        .split(',')
-        .filter(|&x| !x.is_empty())
-        .map(|x| x.trim())
-        .map(|x| i64::from_str_radix(x, 10).unwrap())
-        .collect()
+#[derive(Copy, Clone, Debug, Eq, PartialEq)]
+enum Segment {
+    Top = 0b0000001,
+    TopLeft = 0b0000010,
+    TopRight = 0b0000100,
+    Middle = 0b0001000,
+    BottomLeft = 0b0010000,
+    BottomRight = 0b0100000,
+    Bottom = 0b1000000,
+    None = 0,
 }
 
-#[inline]
-fn compute_cost(crabs: &[i64], mid: i64) -> i64 {
-    let mut cost = 0;
-
-    for &crab in crabs {
-        let distance = (crab - mid).abs();
-
-        cost += distance * (distance + 1) / 2;
-    }
-    cost
+#[derive(Debug, Copy, Clone)]
+struct Display {
+    layout: [Segment; 7],
 }
 
-fn solution(crab_pos: Vec<i64>) -> i64 {
-    // Ternary search on crab_pos
-    // Since the solution will always be somewhere in the middle of
-    // the lowest and highest values present in crab_pos
-    let mut low = std::i64::MAX;
-    let mut high = std::i64::MIN;
+impl From<&str> for Display {
+    fn from(s: &str) -> Self {
+        let mut layout = [Segment::None; 7];
+        for (i, c) in s.chars().enumerate() {
+            let to_push = match c {
+                'a' => Segment::Top,
+                'b' => Segment::TopLeft,
+                'c' => Segment::TopRight,
+                'd' => Segment::Middle,
+                'e' => Segment::BottomLeft,
+                'f' => Segment::BottomRight,
+                'g' => Segment::Bottom,
 
-    for &crab in crab_pos.iter() {
-        low = std::cmp::min(low, crab);
-        high = std::cmp::max(high, crab);
+                _ => unreachable!(),
+            };
+
+            layout[i] = to_push;
+        }
+
+        Self { layout }
     }
+}
 
-    // Break out when the difference is less than 2 because it'll get stuck in a infinite loop
-    while high - low > 2 {
-        let mid1 = low + (high - low) / 3;
-        let mid2 = high - (high - low) / 3;
+fn parse_input(input: &'static str) -> impl Iterator<Item = (Vec<Display>, Vec<Display>)> {
+    input.lines().filter(|&x| !x.is_empty()).map(|x| {
+        let mut x = x.split("|");
+        let hints = x
+            .next()
+            .unwrap()
+            .split(' ')
+            .filter(|x| !x.is_empty())
+            .map(|x| x.into())
+            .collect();
+        let digits = x
+            .next()
+            .unwrap()
+            .split(' ')
+            .filter(|x| !x.is_empty())
+            .map(|x| x.into())
+            .collect();
 
-        let cost1 = compute_cost(&crab_pos, mid1);
-        let cost2 = compute_cost(&crab_pos, mid2);
+        (hints, digits)
+    })
+}
 
-        if cost1 < cost2 {
-            high = mid2;
-        } else {
-            low = mid1;
+fn solution(input: impl Iterator<Item = (Vec<Display>, Vec<Display>)>) -> u64 {
+    let mut answer = 0;
+    for (_, digits) in input {
+        for digit in digits {
+            let set_bits_count = digit.layout.iter().filter(|&&s| s == Segment::None).count();
+
+            match 7 - set_bits_count {
+                2 | 7 | 4 | 3 => answer += 1,
+                _ => (),
+            }
         }
     }
 
-    compute_cost(&crab_pos, (low + high) / 2)
+    answer
 }
 
 fn main() {
@@ -67,9 +94,9 @@ fn main() {
 
 #[bench]
 fn solution_bench(b: &mut test::Bencher) {
-    let input = parse_input(INPUTS[1]);
     b.iter(|| {
-        let result = solution(input.clone());
+        let input = parse_input(INPUTS[1]);
+        let result = solution(input);
         test::black_box(result);
     })
 }
