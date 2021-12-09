2dc1b8af66304e42d783e688b017f55ffaade89a
Ishan Jain <contact@ishanjain.me>
2021-12-09 10:38:37 +0530

    Added Day 9 Part 1

--- src/main.rs
+++ src/main.rs
@@ -6,90 +6,43 @@ const INPUTS: [&'static str; 2] = [
     include_str!("../inputs/input.txt"),
 ];
 
-const fn map_to_segment(c: char) -> u8 {
-    1 << (c as u8 - b'a')
+fn parse_input(input: &'static str) -> Vec<Vec<u8>> {
+    input
+        .lines()
+        .filter(|&x| !x.is_empty())
+        .map(|line| line.bytes().map(|x| x - b'0').collect())
+        .collect()
 }
 
-fn parse_input(input: &'static str) -> impl Iterator<Item = (Vec<u8>, Vec<u8>)> {
-    input.lines().filter(|&x| !x.is_empty()).map(|line| {
-        let (hints, displays) = line.split_once(" | ").unwrap();
+const NEIGHBOURS: [[i32; 2]; 4] = [[-1, 0], [1, 0], [0, -1], [0, 1]];
 
-        let hints = hints
-            .split(' ')
-            .filter(|x| !x.is_empty())
-            .map(|hint| {
-                let mut out = 0;
-                for c in hint.chars() {
-                    out |= map_to_segment(c);
-                }
-                out
-            })
-            .collect();
-
-        let digits = displays
-            .split(' ')
-            .filter(|x| !x.is_empty())
-            .map(|hint| {
-                let mut out = 0;
-                for c in hint.chars() {
-                    out |= map_to_segment(c);
-                }
-                out
-            })
-            .collect();
-
-        (hints, digits)
-    })
-}
-
-fn decode_hint(hints: Vec<u8>) -> [u8; 10] {
-    let mut out = [0; 10];
-
-    for &hint in hints.iter() {
-        match hint.count_ones() {
-            2 => out[1] = hint,
-            4 => out[4] = hint,
-            3 => out[7] = hint,
-            7 => out[8] = hint,
-
-            _ => (),
-        }
-    }
-
-    for hint in hints {
-        let one_overlap = (hint & out[1]).count_ones();
-        let four_overlap = (hint & out[4]).count_ones();
-
-        match (hint.count_ones(), one_overlap, four_overlap) {
-            (5, 1, 2) => out[2] = hint,
-            (5, 2, 3) => out[3] = hint,
-            (5, 1, 3) => out[5] = hint,
-            (6, 1, 3) => out[6] = hint,
-            (6, 2, 4) => out[9] = hint,
-            (6, 2, 3) => out[0] = hint,
-            _ => (),
-        }
-    }
-
-    return out;
-}
-
-fn solution(input: impl Iterator<Item = (Vec<u8>, Vec<u8>)>) -> i32 {
+fn solution(input: Vec<Vec<u8>>) -> i32 {
     let mut answer = 0;
-    for (hints, digits) in input {
-        let decoded_hints = decode_hint(hints);
-        let mut out = 0;
 
-        let offset = digits.len() as u32;
-        for (j, digit) in digits.into_iter().enumerate() {
-            for (i, &hint) in decoded_hints.iter().enumerate() {
-                if hint == digit {
-                    out += i as i32 * 10i32.pow(offset - j as u32 - 1);
+    let m = input.len();
+    let n = input[0].len();
+
+    for i in 0..m {
+        for j in 0..n {
+            let mut is_minimum = true;
+
+            for &neighbour in NEIGHBOURS.iter() {
+                let x = i as i32 + neighbour[0];
+                let y = j as i32 + neighbour[1];
+
+                if x < 0 || y < 0 || x >= m as i32 || y >= n as i32 {
+                    continue;
+                }
+
+                if input[i][j] >= input[x as usize][y as usize] {
+                    is_minimum = false;
                 }
             }
-        }
 
-        answer += out;
+            if is_minimum {
+                answer += input[i][j] as i32 + 1;
+            }
+        }
     }
 
     answer
@@ -105,9 +58,9 @@ fn main() {
 
 #[bench]
 fn solution_bench(b: &mut test::Bencher) {
+    let input = parse_input(INPUTS[1]);
     b.iter(|| {
-        let input = parse_input(INPUTS[1]);
-        let result = solution(input);
+        let result = solution(input.clone());
         test::black_box(result);
     })
 }
